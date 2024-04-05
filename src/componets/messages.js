5a819f148270ef7c9bdaee8298306b65a9f3e3b3
Raju Thota <rajuthota82@gmail.com>
2024-04-05 12:19:45 +0530

    uptaed

--- src/componets/messages.js
+++ src/componets/messages.js
@@ -1,7 +1,7 @@
  
 
  import React, { useContext, useEffect, useState } from 'react'
- import Message from '../Message'
+ import Message from './Message'
 import { doc, onSnapshot } from 'firebase/firestore'
 import { ChatContext } from '../context/chatcontext'
 import { db } from '../firebase'
